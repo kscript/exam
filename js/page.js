d3e159059e7609a4e8e5de0efa8d98f750961e96
kscript <1258@live.cn>
2017-03-02 15:33:54 +0800

    测试

--- js/page.js
+++ js/page.js
@@ -1,6 +1,6 @@
 ;(function(){
 	var a=document.createElement("a");
-	a.href=(page.id+1)+".html";
+	a.href="https://kscript.github.io/exam/Q/"+(page.id+1)+".html";
 	a.innerHTML="下一题";
-	document.body.appendChild("a");
+	document.body.appendChild(a);
 })()
