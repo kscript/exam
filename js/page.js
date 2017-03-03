448ca1bfcd5e88fe2dc0dbdb931c4b0765a7dfbb
kscript <1258@live.cn>
2017-03-03 11:23:24 +0800

    链接固定

--- js/page.js
+++ js/page.js
@@ -1,6 +1,7 @@
 ;(function(){
 	var a=document.createElement("a");
-	a.href="https://kscript.github.io/exam/Q/"+(page.id+1)+".html";
+	a.href="https://kscript.github.io/exam/Q/"+(page.id+1)+".html";	
+	a.style.cssText="position:fixed;top:20px;left:auto;"
 	a.innerHTML="下一题";
 	document.body.appendChild(a);
 })()
