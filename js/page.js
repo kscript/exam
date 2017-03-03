d868c8e705d6ea5636d1581080b1a9d0c9ca0e2d
kscript <2046@live.cn>
2017-03-03 13:24:21 +0800

    新增

--- js/page.js
+++ js/page.js
@@ -1,7 +1,24 @@
 ;(function(){
-	var a=document.createElement("a");
-	a.href="https://kscript.github.io/exam/Q/"+(page.id+1)+".html";	
-	a.style.cssText="position:fixed;top:20px;left:auto;"
-	a.innerHTML="下一题";
-	document.body.appendChild(a);
+
+	creatEle.apply(null,["a",{
+		href:"https://kscript.github.io/exam/Q/"+(page.id+1)+".html",
+		style:{
+			cssText:"position:fixed;top:20px;left:auto;"
+		},
+		innerHTML:"下一题"
+	},{
+		href:page.id==1?"javascript:void(0);":"https://kscript.github.io/exam/Q/"+(page.id-1)+".html",
+		style:{
+			cssText:"position:fixed;top:20px;left:auto;"
+		},
+		innerHTML:page.id==1"没有啦":"上一题"
+	}])
+
+	function creatEle(tagname,conf){
+		var el=document.createElement(tagname);
+		for(var i in conf){
+			el[i]=conf.i;
+		}
+		(parent||document.body).appendChild(el);
+	}
 })()
